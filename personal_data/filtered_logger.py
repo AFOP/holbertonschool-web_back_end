90cdc6526071fb2a1d2c23d37db9d3e2c3b814db
AFOP <felipe@ASUSAFOP.localdomain>
2023-05-30 21:00:38 -0500

    [UPDATE] task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -12,7 +12,8 @@ Return: the log message obfuscated
 
 import re
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: str, redaction: str, message: str,
+                 separator: str) -> str:
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
