b4e3083f7a7f6bdf113be2191eae1b0c4906592c
AFOP <felipe@ASUSAFOP.localdomain>
2023-05-30 21:10:39 -0500

    [UPDATE] task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """
 filter_datum: returns the log message obfuscated
-Arguments: 
+Arguments:
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
     message: a string representing the log line
-    separator: a string representing by which character is separating all fields in the log line (message)
+    separator: a string representing by which character
+    is separating all fields in the log line (message)
 Return: the log message obfuscated
 """
 
@@ -13,8 +14,10 @@ Return: the log message obfuscated
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """ returns the log message obfuscated """
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
