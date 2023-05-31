7e7c081a03de2167ef5e1ea138dcb119b6386d45
AFOP <felipe@ASUSAFOP.localdomain>
2023-05-30 21:25:43 -0500

    [UPDATE] task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 """
-filter_datum: returns the log message obfuscated
-Arguments:
-    fields: a list of strings representing all fields to obfuscate
-    redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character
-    is separating all fields in the log line (message)
-Return: the log message obfuscated
+    RedactingFormatter
 """
 
 
+import logging
 import re
 from typing import List
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ init """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format record """
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -21,3 +37,14 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return re.sub(r"(\w+)=([a-zA-Z0-9@\.\-\(\)\ \:\^\<\>\~\$\%\@\?\!\/]*)",
                   lambda match: match.group(1) + "=" + redaction
                   if match.group(1) in fields else match.group(0), message)
+
+
+def get_logger() -> logging.Logger:
+    """ return a logger object """
+    lg = logging.getLogger("user_data")
+    lg.setLevel(logging.INFO)
+    lg.propagate = False
+    sh = logging.StreamHandler()
+    sh.setFormatter(RedactingFormatter(PII_FIELDS))
+    lg.addHandler(sh)
+    return lg
