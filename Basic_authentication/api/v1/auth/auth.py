f157651e40b2a3151a14c545bad46736b8077df1
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-01 18:30:52 -0500

    [ADD] task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,18 @@ class Auth:
             Function require_auth
             Return: Fase
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path.endswith('/'):
+            path = path[:-1]
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                excluded_path = excluded_path[:-1]
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
