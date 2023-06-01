c4621a0681f141faa68089691f07f9d010b43ab2
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-01 13:11:15 -0500

    [ADD] task 1

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -3,6 +3,10 @@
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
+from flask import Blueprint, abort
+
+
+unauthorized_bp = Blueprint('unauthorized_bp', __name__)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -24,3 +28,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+    """ GET /api/v1/unauthorized
+    Return:
+      - 401
+    """
+    return abort(401)
