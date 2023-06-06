1fa8a61222f61f633bfe3efbb11b60e93957c511
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-06 13:34:39 -0500

    [UPDATE] task 4 and README

--- Session_authentication/main_1.py
+++ Session_authentication/main_1.py
@@ -1,32 +1,28 @@
-#!/usr/bin/python3
-""" Check response
+#!/usr/bin/env python3
+""" Main 1
 """
-import requests
+from api.v1.auth.session_auth import SessionAuth
 
-if __name__ == "__main__":
-    r = requests.get('http://0.0.0.0:3456/api/v1/status')
-    if r.status_code != 200:
-        print("Wrong status code: {}".format(r.status_code))
-        exit(1)
-    if r.headers.get('content-type') != "application/json":
-        print("Wrong content type: {}".format(r.headers.get('content-type')))
-        exit(1)
-    
-    try:
-        r_json = r.json()
-        
-        if len(r_json.keys()) != 1:
-            print("Not the right number of element in the JSON: {}".format(r_json))
-            exit(1)
-        
-        status_value = r_json.get('status')
-        if status_value is None:
-            print("Missing 'status' key in the JSON: {}".format(r_json))
-            exit(1)
-        if status_value != "OK":
-            print("'status' doesn't have the right value: {}".format(status_value))
-            exit(1)
-            
-        print("OK", end="")
-    except:
-        print("Error, not a JSON")
+sa = SessionAuth()
+
+print("{}: {}".format(type(sa.user_id_by_session_id), sa.user_id_by_session_id))
+
+user_id = None
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = 89
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "abcde"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "fghij"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
+
+user_id = "abcde"
+session = sa.create_session(user_id)
+print("{} => {}: {}".format(user_id, session, sa.user_id_by_session_id))
