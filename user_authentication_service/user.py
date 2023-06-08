7712c8fca40f3a2f33135ef639bc205f11faf42a
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-07 19:51:58 -0500

    [UPDATE] task 0

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -9,7 +9,11 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class User(Base):
+    """
+    class User base sqlalchemy
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
