d9876be01add7fb17080d54054efd9a6ef6a7991
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-14 13:54:58 -0500

    [UPDATE] test_utils.py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -9,12 +9,17 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
-        ({}, ("a",), "KeyError: 'a'"),
-        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
+    @parameterized.expand([
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
+    ])
     def test_access_nested_map_exception(self, nested_map, path, expected_message):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
