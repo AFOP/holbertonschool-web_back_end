f1ae61bfb27fa40b972651ccdf1d693cc05981d3
AFOP <felipe@ASUSAFOP.localdomain>
2023-06-14 14:02:15 -0500

    [UPDATE] test_utils.py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python3
-"""test utils
-"""
-import unittest
+""" Test SUITE Unittest module Task """
+
+from unittest import TestCase, mock
+from unittest.mock import patch, Mock
 from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
 
-from utils import access_nested_map
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """ Class for testing Nested Map function """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+    def test_access_nested_map(self, map, path, expected_output):
+        """ Test method return output """
+        real_output = access_nested_map(map, path)
+        self.assertEqual(real_output, expected_output)
 
-     @parameterized.expand([
+    @parameterized.expand([
         ({}, ("a",), 'a'),
         ({"a": 1}, ("a", "b"), 'b')
     ])
