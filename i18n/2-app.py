bb5fe12c5e8619e66aa1ae8e03dba7e774ce8bb0
AFOP <felipe@ASUSAFOP.localdomain>
2024-01-11 15:53:18 -0500

    [UPDATE] task 2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,44 +1,45 @@
 #!/usr/bin/env python3
 """
-Flask app
+Module documentation: 2-app.py
 """
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
-class Config(object):
-    """
-    Config for languages
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
 app = Flask(__name__)
+
+# Instantiate the Babel object
 babel = Babel(app)
 
+class Config:
+    """
+    Config class documentation
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
-class Config(object):
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
+# Elimina la instancia de Babel en el nivel del módulo
+# babel = Babel(app)
 
 app.config.from_object(Config)
 
-@app.route('/', methods=["GET"], strict_slashes=False)
-def index():
-    """
-    Return index
-    """
-    return render_template('2-index.html')
-
+# Mueve la definición de get_locale antes de la llamada a init_app
 @babel.localeselector
 def get_locale():
     """
-    Get locale selector for babel
+    Locale selector documentation
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 babel.init_app(app, locale_selector=get_locale)
+
+@app.route('/')
+def index():
+    """
+    Route documentation: /
+    """
+    return render_template('2-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
