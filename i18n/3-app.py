3b603dd6d7a00231238fcd0c08be8e47ae445aa8
AFOP <felipe@ASUSAFOP.localdomain>
2024-01-11 16:07:29 -0500

    [UPDATE] task 3

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,36 +1,40 @@
 #!/usr/bin/env python3
 """
-Flask app
+Module documentation: 3-app.py
 """
+
 from flask import Flask, render_template, request
-from flask_babel import Babel
-
-
-class Config(object):
-    """
-    Config for languages
-    """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
+from flask_babel import Babel, _  # Importar la función de traducción
 
 app = Flask(__name__)
-babel = Babel(app)
+
+# Instantiate the Babel object
+babel = Babel()
+
+class Config:
+    """
+    Config class documentation
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 app.config.from_object(Config)
-
-
-@app.route('/', methods=["GET"], strict_slashes=False)
-def index():
-    """
-    Return index
-    """
-    return render_template('3-index.html')
-
+babel.init_app(app)
 
 @babel.localeselector
 def get_locale():
     """
-    Get locale selector for babel
+    Locale selector documentation
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """
+    Route documentation: /
+    """
+    return render_template('3-index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
